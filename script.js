80f646c15b77e9a42163d3bb8513d54b0ca49ac6
Lautaro Bertarelli <lautaber9@gmail.com>
2023-10-19 20:42:42 -0300

    last i think

---
+++ script.js
@@ -0,0 +1,15 @@
+// let Person = {
+//   firstName: '',
+//   lastName: '',
+//   age: '',
+//   getName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+
+// let person1 = {
+//   firstName: 'Lautaro',
+//   lastName: 'Bertarelli'
+// }
+// Object.setPrototypeOf(person1, Person);
+// console.log(person1.getName());
